6910ac5234f0fcc568cb58b38b4f88b7e253f8b7
Joshua OBannon <jtobannon@gmail.com>
2020-11-28 21:07:19 -0700

    Commit Fish Files

---
+++ config.fish
@@ -0,0 +1,23 @@
+starship init fish | source
+fish_vi_key_bindings
+
+## Josh bindings
+alias ..='cd ..'
+alias la='ls -laFGgohq'
+alias l='ls'
+alias gs='git status'
+alias ga='git add'
+alias gco='git checkout'
+alias be='bundle exec'
+
+
+
+
+alias ww='cd /Users/joshuaobannon/Documents/turing/5module_ressurrect/5module'
+alias fs='cd /Users/joshuaobannon/Documents/bangequals/findsweeps'
+alias sjn='cd /Users/joshuaobannon/Documents/bangequals/solutions_journalism'
+alias jto='cd /Users/joshuaobannon/Documents/'
+ 
+alias sourcefish='source ~/.config/fish/config.fish'
+
+
